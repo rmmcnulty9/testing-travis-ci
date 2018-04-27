ad0af9c8aa1efd9edc1f8278cb407cf5f16c6e82
Ryan <rmmcnulty9@gmail.com>
2018-04-27 10:43:37 -0400

    Add a failing test.

--- build.sh
+++ build.sh
@@ -11,4 +11,13 @@ docker run -d --rm --name my_container "$TARGET" bash -c "tail -f /dev/null"
 echo "Running mock tests"
 docker exec my_container bash -c "sleep 20s"
 
+
 docker stop my_container
+
+if [ "$TARGET" == "b" ]; then
+
+    echo "You can't be build b!"
+    exit 1
+fi
+
+exit 0
